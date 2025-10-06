69588555302d4b9f093fb052ecb76496277f19d0
Mateus Ortiz <69127852+MateusOF5512@users.noreply.github.com>
2025-10-06 20:52:29 -0300

    Update home.py

--- Pages/home.py
+++ Pages/home.py
@@ -10,8 +10,11 @@ O Caderno da Turma tem como objetivo principal melhorar a forma como a turma de 
 Com o apoio da MonitorIA, o chatbot com inteligência artificial integrado aos cadernos, os registros feitos pelos alunos deixam de ser apenas anotações e passam a servir também como fonte para revisões, esclarecimentos de dúvidas e apoio ao estudo individual. Assim, o projeto busca resolver a falta de organização e de colaboração no processo de aprendizagem, tornando o estudo mais dinâmico, acessível e conectado entre os colegas.
 
 '''
-
+st.markdown('')
+st.markdown('')
 st.markdown(TEXTO)
-
+st.markdown('')
+st.markdown('')
 st.divider()
+
 
