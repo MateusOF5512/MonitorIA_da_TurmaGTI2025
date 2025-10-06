e735ecc629b40950f61dd4c10b1bb7f90cfb3582
Mateus Ortiz <69127852+MateusOF5512@users.noreply.github.com>
2025-10-06 20:57:28 -0300

    Update home.py

--- Pages/home.py
+++ Pages/home.py
@@ -6,7 +6,7 @@ st.subheader("🏠 Bem vindo(a)  ***"+conta+"***  ao Caderno da Turma", divider=
 
 
 TEXTO = '''
-O projeto **Caderno da Turma GTI** tem como objetivo desenvolver um **caderno digital coletivo** para os alunos da segunda fase do curso de Gestão da Tecnologia da Informação (GTI 2025). A proposta busca **centralizar anotações, materiais e recursos de estudo em um ambiente colaborativo**, enfrentando desafios como a dispersão de informações e a falta de organização dos registros acadêmicos. O sistema pretende oferecer uma solução simples, moderna e integrada ao contexto da turma, promovendo a *troca de conhecimento e fortalecendo o aprendizado coletivo*.
+O projeto **Caderno da Turma GTI** tem como objetivo desenvolver um **caderno digital coletivo** para os alunos da segunda fase do curso de Gestão da Tecnologia da Informação (GTI 2025). A proposta busca **centralizar anotações, materiais e recursos de estudo em um ambiente colaborativo**, enfrentando desafios como a dispersão de informações e a falta de organização dos registros acadêmicos. O sistema pretende oferecer uma solução simples, moderna e integrada ao contexto da turma, promovendo a **troca de conhecimento e fortalecendo o aprendizado coletivo**.
 
 O projeto será restrito à turma de GTI 2025 e operará dentro das limitações das ferramentas gratuitas utilizadas, assegurando viabilidade e execução completa. O desenvolvimento seguirá etapas que incluem diagnóstico, definição de requisitos, construção, testes e implantação final, com acompanhamento inicial do uso. Seu desempenho será avaliado por métricas como engajamento, frequência de acesso, volume de anotações, interações com a IA e satisfação dos usuários, permitindo mensurar o impacto do caderno digital na organização, colaboração e aprendizado da turma.
 
@@ -18,6 +18,7 @@ st.markdown(TEXTO)
 st.markdown('')
 st.markdown('')
 st.divider()
+
 
 
 
